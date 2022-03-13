770b0633320a2c39468b4694a441804dcd1db3df
박희태 <heetae.park@yanolja.com>
2022-03-13 23:25:53 +0900

    프로젝트 init

---
+++ Sweat-SwiftUI/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Sweat-SwiftUI
+//
+//  Created by heetae.park on 2022/03/13.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Sweat-SwiftUI/Sweat_SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  Sweat_SwiftUIApp.swift
+//  Sweat-SwiftUI
+//
+//  Created by heetae.park on 2022/03/13.
+//
+
+import SwiftUI
+
+@main
+struct Sweat_SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
