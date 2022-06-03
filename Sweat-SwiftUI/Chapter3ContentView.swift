0d88809eb2477ee81ee3af0dee06c5ed92889c04
박희태 <>
2022-06-03 23:56:38 +0900

    NavigationView bar Item

--- Sweat-SwiftUI/Chapter3ContentView.swift
+++ Sweat-SwiftUI/Chapter3ContentView.swift
@@ -9,9 +9,53 @@ import SwiftUI
 
 struct Chapter3ContentView: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
-            .navigationTitle("Chapter 3")
-            .navigationBarTitleDisplayMode(.inline)
+        VStack(spacing: 20) {
+            HStack(spacing: 20) {
+                Button("Button") {
+                    print("Button1")
+                }
+                Button(action: { print("Button2") }) {
+                    Text("Button")
+                        .padding()
+                        .background(RoundedRectangle(cornerRadius: 10).strokeBorder())
+                }
+                Button(action: { print("Button3") }) {
+                    Circle()
+                        .stroke(lineWidth: 2)
+                        .frame(width: 80, height: 80)
+                        .overlay(Text("Button"))
+                }
+                .tint(.green)
+            }
+            HStack(spacing: 20) {
+                Button(action: { print("Button1") }) {
+                    Image("fig")
+                        .resizable()
+                        .frame(width: 120, height: 120)
+                }
+                Button(action: { print("Button2") }) {
+                    Image(systemName: "play.circle")
+                        .imageScale(.large)
+                        .font(.largeTitle)
+                }
+                .tint(.green)
+            }.padding()
+            VStack(spacing: 30) {
+                Button("DefaultButtonStyle") {}
+                    .buttonStyle(DefaultButtonStyle())
+                Button("BorderlessButtonStyle") {}
+                    .buttonStyle(.borderless)
+                Button("PlainButtonStyle") {}
+                    .buttonStyle(.plain)
+            }
+            Image(systemName: "person.circle")
+                .imageScale(.large)
+                .onTapGesture { print("onTapGesture") }
+            Button(action: { print("버튼") }) {
+                Image(systemName: "person.circle")
+                    .imageScale(.large)
+            }
+        }
     }
 }
 
--- Sweat-SwiftUI/ContentView.swift
+++ Sweat-SwiftUI/ContentView.swift
@@ -9,10 +9,22 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        NavigationView {
+        let leadingItem: Button = Button(action: { print("Leading item tapped") }) {
+            Image(systemName: "bell").imageScale(.large)
+        }
+        let trailingItem = HStack {
+            Button(action: { print("Trailing") }) {
+                Image(systemName: "gear").imageScale(.large)
+            }
+            Button(action: { print("Trailing") }) {
+                Image(systemName: "pencil").imageScale(.large)
+            }
+        }
+        
+        return NavigationView {
             VStack(spacing: 30) {
                 NavigationLink("과일마트") {
-                    Home()
+                    Home().navigationBarBackButtonHidden(true)
                 }
                 NavigationLink("Chapter 2") {
                     Chapter2ContentView()
@@ -20,6 +32,10 @@ struct ContentView: View {
                 NavigationLink("Chapter 3") {
                     Chapter3ContentView()
                 }
+            }
+            .toolbar {
+                ToolbarItem(placement: .navigationBarLeading) { leadingItem }
+                ToolbarItem(placement: .navigationBarTrailing) { trailingItem }
             }
             .navigationTitle("ContentView")
             .navigationBarTitleDisplayMode(.inline)
